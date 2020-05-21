4109352efbec7971fa419c0424cbdf86011c3eb5
AlbyFer <ferrando2009@gmail.com>
2020-05-21 13:27:05 +0200

    New problem

---
+++ Problem6/problem_6.cpp
@@ -0,0 +1,43 @@
+//
+//  problem_6.cpp
+//  
+//
+//  Created by Alberto Ferrando on 21/05/2020.
+//
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::cout << "Works" << "\n";
+}
+
+
+
+/*
+int main() {
+    unsigned long sum_of_squares = 0;
+    unsigned long square;
+    unsigned long square_of_sum = 0;
+    
+    
+    for(int i=0; i<=100; i++) {
+        std::cout << i << "\n";
+        square = i*i;
+        sum_of_squares = sum_of_squares + square;
+    }
+    
+    std::cout << "The sum of squares is: " << sum_of_squares << "\n";
+    
+    for(int i=0; i<=100; i++) {
+        std::cout << i << "\n";
+        square_of_sum = square_of_sum + i;
+    }
+    square_of_sum = square_of_sum * square_of_sum;
+    
+    std::cout << "The square of the sum is: " << square_of_sum << "\n";
+    
+    std::cout << "Their difference is: " << square_of_sum - sum_of_squares << "\n";
+    
+}
+*/
